29fd4322fa0c83801a2020354cc576837d649398
seungjae6874 <qkrtmdwo1@gmail.com>
2020-01-13 17:33:07 +0900

    2020-01-03 image.py ver 1.2

--- image.py
+++ image.py
@@ -4,7 +4,7 @@ import os
 from scipy.misc import imread, imresize
 import matplotlib.pyplot as plt
 print ("PACKAGES LOADED")
-
+'''
 #Print Current Folder
 cwd = os.getcwd()
 print("Current folder is %s" %(cwd))
@@ -53,4 +53,50 @@ plt.title("Resize cat small")
 plt.show()
 
 
+#Grayscale
 
+def rgb2gray(rgb):
+    if len(rgb.shape) is 3:
+        return np.dot(rgb[...,:3], [0.299 , 0.587, 0.114])
+    else:
+        print("Current image is already gray!")
+        return rgb
+
+
+catsmallgray = rgb2gray(catsmall)
+print("size of catsmall is %s" % (catsmallgray.shape,))
+print("type of catsmallgray is", type(catsmallgray))
+
+plt.imshow(catsmallgray, cmap=plt.get_cmap("gray"))
+plt.title("[imshow] Gray image")
+plt.colorbar()
+plt.show()
+'''
+
+#see what inside in this folder
+cwd = os.getcwd()
+path = cwd
+flist = os.listdir(path)
+print("[%d] FILE ARE IN [%s]" %(len(flist),path))
+
+for i,f in enumerate(flist):
+    print("[%d] The file is [%s]" %(i,f))
+
+
+valid_exts = [".jpg",".gif",".png",".jpeg"]
+imgs = []
+names = []
+for f in flist:
+    ext = os.path.splitext(f)[1]
+    if ext.lower() not in valid_exts:
+        continue
+    fullpath = os.path.join(path,f)
+    imgs.append(imread(fullpath))
+    names.append(os.path.splitext(f)[0])
+    
+
+for img,name in zip(imgs,names):
+    plt.imshow(img)
+    plt.title(name)
+    plt.show()
+    
