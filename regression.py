e7b54143290b0175ad37f17c7e0845e3fdb981ba
seungjae6874 <qkrtmdwo1@gmail.com>
2020-01-16 15:45:48 +0900

    2020-01-16 regression ver 1.1

--- regression.py
+++ regression.py
@@ -5,3 +5,11 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
 print("Packages Loaded")
+
+
+mnist = input_data.read_data_sets('data/', one_hot = True)
+trainimg = mnist.train.images
+trainlabel = mnist.train.labels
+testimg = mnist.test.images
+testlabel = mnist.test.labels
+print("Mnist Loaded")
